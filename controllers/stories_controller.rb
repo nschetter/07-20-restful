bff9ac6b024a8d6e7c6d5430cfc093f4082eaf4f
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-20 15:16:32 -0500

    Edit story view

--- controllers/stories_controller.rb
+++ controllers/stories_controller.rb
@@ -1,5 +1,6 @@
 # all stories for a user
 get "/users/:id/stories" do
+  @user = User.find(params["id"])
   @stories = User.find(params["id"]).stories
   erb :"/stories/index"
 end
@@ -13,5 +14,18 @@ post "/users/:id/stories" do
   @story = Story.create({"name" => params["story"]["name"]})
   @user = User.find(params["id"])
   @user.stories << @story
+  redirect "/users"
+end
+
+get "/users/:id/stories/:story_id/edit" do
+  @user = User.find(params["id"])
+  @story = Story.find(params["story_id"])
+  erb :"/stories/edit"
+end
+
+put "/users/:id/stories/:story_id" do
+  @story = Story.find(params["story_id"])
+  @story.name = params["story"]["name"]
+  @story.save
   redirect "/users"
 end
