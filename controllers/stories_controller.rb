8fe89e08294eb3a60b81da0aec63a5f700516032
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-20 15:06:57 -0500

    Add story for user

--- controllers/stories_controller.rb
+++ controllers/stories_controller.rb
@@ -1,8 +1,17 @@
+# all stories for a user
 get "/users/:id/stories" do
   @stories = User.find(params["id"]).stories
   erb :"/stories/index"
 end
 
-get "/users/:id/stories/:id" do
-  
+get "/users/:id/stories/new" do
+  @user = User.find(params["id"])
+  erb :"/stories/new"
+end
+
+post "/users/:id/stories" do
+  @story = Story.create({"name" => params["story"]["name"]})
+  @user = User.find(params["id"])
+  @user.stories << @story
+  redirect "/users"
 end
